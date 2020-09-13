1fb9f216ec86fe9b281dd8b92af2746080177d16
decute <manish@anom.io>
2020-09-14 00:15:27 +0530

    aaa

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -7,10 +7,24 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbEndpoint;
+import android.hardware.usb.UsbInterface;
+import android.hardware.usb.UsbManager;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
 /**
  * This class echoes a string called from JavaScript.
  */
 public class MathCalculator extends CordovaPlugin {
+
+    
+    HashMap<String, UsbDevice> connectedDevices = null;
+     /* USB system service */
+    private UsbManager mUsbManager;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -26,11 +40,33 @@ public class MathCalculator extends CordovaPlugin {
     }
 
     private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+            mUsbManager = (UsbManager) cordova.getActivity().getSystemService(UsbManager.class);
+            connectedDevices = mUsbManager.getDeviceList();
+
+            if (connectedDevices.isEmpty()) {
+                callbackContext.success("No Devices Currently Connected");
+            }else{
+            Iterator<UsbDevice> deviceIterator = connectedDevices.values().iterator();
+            JSONObject deviceObj = null;
+            JSONArray devicesArray = new JSONArray();
+            while (deviceIterator.hasNext()) {
+                    UsbDevice device = deviceIterator.next();  
+                    try{
+                    deviceObj = new JSONObject();    
+                    deviceObj.put("DeviceID", device.getDeviceId());
+                    deviceObj.put("DeviceName", device.getDeviceName());
+                    deviceObj.put("DeviceClass", device.getDeviceClass());
+                    deviceObj.put("VendorID", device.getVendorId());
+                    deviceObj.put("ProductID", device.getProductId());
+                    deviceObj.put("InterfaceCount", device.getInterfaceCount());    
+                    devicesArray.put(deviceObj); 
+                    } catch (JSONException e) {
+                     // TODO Auto-generated catch block                        
+                       callbackContext.error("Failed to bind Device Information");
+                    } 
+              }
+                callbackContext.success(devicesArray);
+            }
     }
 
     private void add(JSONArray args, CallbackContext callbackContext) {
