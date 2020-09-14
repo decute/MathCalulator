c7eec057631e9fbd8fadf2c12aabe0b8ae879c72
decute <manish@anom.io>
2020-09-14 12:58:14 +0530

    plugin updated

--- src/android/MathCalculator.java
+++ src/android/MathCalculator.java
@@ -25,6 +25,16 @@ public class MathCalculator extends CordovaPlugin {
     HashMap<String, UsbDevice> connectedDevices = null;
      /* USB system service */
     private UsbManager mUsbManager;
+    private UsbDevice deviceFound = null;
+    private UsbDeviceConnection connection;
+    private UsbInterface usbInterface = null;
+    private UsbEndpoint endpointRead = null;
+    private UsbEndpoint endpointWrite =null;
+
+    private int targetVendorID= 1972;
+    private int targetProductID = 144;
+    int lTIMEOUT = 2000;
+
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -32,14 +42,46 @@ public class MathCalculator extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
-        } else if(action.equals("add")) {
+        }else if(action.equals("add")) {
             this.add(args, callbackContext);
+            return true;
+        }else if(action.equals("findUsbDevices")) {
+            this.findUsbDevices(callbackContext);
+            return true;
+        }else if(action.equals("setUsbDevice")) {
+            this.setUsbDevice(args, callbackContext);
+            return true;
+        }else if(action.equals("openUsbConnection")) {
+            this.openUsbConnection(callbackContext);
+            return true;
+        }else if(action.equals("closeUsbConnection")) {
+            this.closeUsbConnection(callbackContext);
+            return true;
+        }else if(action.equals("sendCommand")) {
+            String command = args.getString(0);
+            this.sendCommand(command,callbackContext);
+            return true;
+        }else if(action.equals("getUsbDevice")) {
+            this.getUsbDevice(callbackContext);
             return true;
         }
         return false;
     }
 
+    private void getUsbDevice(CallbackContext callbackContext) {     
+            callbackContext.success("PID AND VID IS"+targetProductID + "-----"+targetVendorID);
+    }
+
     private void coolMethod(String message, CallbackContext callbackContext) {
+           if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+
+     private void findUsbDevices(CallbackContext callbackContext) {
+
             mUsbManager = (UsbManager) cordova.getActivity().getSystemService(UsbManager.class);
             connectedDevices = mUsbManager.getDeviceList();
 
@@ -67,6 +109,104 @@ public class MathCalculator extends CordovaPlugin {
               }
                 callbackContext.success(devicesArray);
             }
+    }
+
+    
+    private void setUsbDevice(JSONArray args, CallbackContext callbackContext) {
+            if(args != null){
+             try{
+                   int pid =  Integer.parseInt(args.getJSONObject(0).getString("pid"));
+                   int vid =  Integer.parseInt(args.getJSONObject(0).getString("vid"));
+                   targetVendorID = vid;
+                   targetProductID = pid;
+                   callbackContext.success("success");
+             } catch(Exception ex) {
+                callbackContext.error("Something went wrong" + ex);
+             }
+
+        } else {
+            callbackContext.error("Please don't pass null value. ");
+        }
+    }
+
+    private void openUsbConnection(CallbackContext callbackContext) {
+            mUsbManager = (UsbManager) cordova.getActivity().getSystemService(UsbManager.class);
+            connectedDevices = mUsbManager.getDeviceList();
+            if (connectedDevices.isEmpty()) {
+                callbackContext.success("No Devices Currently Connected");
+            }else{
+            Iterator<UsbDevice> deviceIterator = connectedDevices.values().iterator();
+            while (deviceIterator.hasNext()) {
+                UsbDevice device = deviceIterator.next();
+                if(device.getVendorId()==targetVendorID){
+                if(device.getProductId()==targetProductID){
+                        deviceFound = device;
+                   }
+               }           
+           }
+           if(deviceFound != null){
+                getPermission(deviceFound); 
+                try{
+                    connection = mUsbManager.openDevice(device);
+                    callbackContext.success("Connection Established Successfully");   
+                }catch (Exception e) {
+                    callbackContext.error("Failed to Established the Connection");
+                } 
+           }else{
+                    callbackContext.error("No Device Found of PID AND VID Tyep"+ targetProductID + "----"+ targetVendorID );
+           }      
+         }
+    }
+
+    private void sendCommand(String command, CallbackContext callbackContext) {   
+        if(deviceFound==null){
+            callbackContext.error("Please Open the USB Connection First");
+        }else {
+           getPermission(deviceFound);
+           if(connection!=null){
+                usbInterface = device.getInterface(0x01);
+                connection.claimInterface(usbInterface, true);
+                byte[] data = new byte[8];
+                connection.controlTransfer(0x21, 0x22, 0x03, 0x01, data, 0, lTIMEOUT);
+                byte[] buffer = {0x00, (byte) 0xC2, 0x01, 0x00, 0x00, 0x00, 0x08};
+                connection.controlTransfer(0x21, 0x20, 0x00, 0x01, buffer, buffer.length, lTIMEOUT);
+                endpointRead  = usbInterface.getEndpoint(0x00);
+                endpointWrite = usbInterface.getEndpoint(0x01);
+                String command = "ISN?\r\n";
+                byte[] buf = command.getBytes(StandardCharsets.UTF_8);
+                int dataLength = buf.length;
+                int res = connection.bulkTransfer(endpointWrite,buf, dataLength, lTIMEOUT);
+                callbackContext.success("Bulk Transfer write, data_len:" + dataLength + "==" + res);
+                byte[] sn_data = new byte[64];
+                try {
+                   connection.bulkTransfer(endpointRead, sn_data, sn_data.length, lTIMEOUT);
+                }
+                catch (Exception e) {
+                callbackContext.error("Bulk Transfer Failed");
+                }
+           }else{
+               callbackContext.error("Please Open the USB Connection First");
+           }
+        }
+    }
+
+    private void closeUsbConnection(CallbackContext callbackContext) {
+         if(connection==null){
+             callbackContext.error("Failed to Close the USB Connection");
+         }else{
+             connection.releaseInterface(usbInterface);
+             connection.close();
+             deviceFound = null;
+             usbInterface = null;
+             endpointRead = null;
+             endpointWrite =null;
+         }
+    }
+
+    private void getPermission(UsbDevice device) {
+         if (!mUsbManager.hasPermission(device)) {
+             mUsbManager.requestPermission(device, PendingIntent.getActivity(cordova.getActivity(), 0, new Intent("android.permission.MANAGE_USB"), 0)) ;
+           }
     }
 
     private void add(JSONArray args, CallbackContext callbackContext) {
--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -7,3 +7,35 @@ module.exports.coolMethod = function (arg0, success, error) {
 module.exports.add = function (arg0, success, error) {
     exec(success, error, 'MathCalculator', 'add', [arg0]);
 };
+
+//This will give information about the list of device connected too
+module.exports.findUsbDevices = function (success, error) {
+    exec(success, error, 'MathCalculator', 'findUsbDevices', []);
+};
+
+//with the help of productId and VendorID , usb connection will be setup
+module.exports.setUsbDevice = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'setUsbDevice', [arg0]);
+};
+
+//This method will help us to close all the connection.
+module.exports.openUsbConnection = function (success, error) {
+    exec(success, error, 'MathCalculator', 'openUsbConnection', []);
+};
+
+//This method will help us to close all the connection.
+module.exports.closeUsbConnection = function (success, error) {
+    exec(success, error, 'MathCalculator', 'closeUsbConnection', []);
+};
+
+
+//with the help of productId and VendorID , send Remote command
+module.exports.sendCommand = function (arg0, success, error) {
+    exec(success, error, 'MathCalculator', 'sendCommand', [arg0]);
+};
+
+module.exports.getUsbDevice = function (success, error) {
+    exec(success, error, 'MathCalculator', 'getUsbDevice', []);
+};
+
+
