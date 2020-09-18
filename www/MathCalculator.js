6901d64ff1fd0b7b0baa7a49ecb020215c74ef65
decute <manish@anom.io>
2020-09-18 15:22:21 +0530

    function added

--- www/MathCalculator.js
+++ www/MathCalculator.js
@@ -1,61 +1,57 @@
-var exec = require('cordova/exec');
+var MathCalculator = {
 
-module.exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'coolMethod', [arg0]);
-};
+    coolMethod: function (arg0, success, error) {
+        cordova.exec(
+            success, 
+            error, 
+            'MathCalculator', 
+            'coolMethod', 
+            [arg0]);
+    },
+    add : function (arg0, success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'add', [arg0]);
+    },
+    findUsbDevices : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'findUsbDevices', []);
+    },
+    setUsbDevice : function (arg0, success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'setUsbDevice', [arg0]);
+    },
+    openUsbConnection : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'openUsbConnection', []);
+    },  
+    closeUsbConnection : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'closeUsbConnection', []);
+    },
+    sendCommand : function (arg0, success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'sendCommand', [arg0]);
+    },
+    sendCommandAndWaitResponse : function (arg0, success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'sendCommandAndWaitResponse', [arg0]);
+    },
+    getUsbDevice : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'getUsbDevice', []);
+    },
+    isUsbDeviceConnected : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'isUsbDeviceConnected', []);
+    },
+    openAscanUsbConnection : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'openAscanUsbConnection', []);
+    },
+    getAscan : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'getAscan', []);
+    },
+    testFunction : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'testFunction', []);
+    },
+    testThreadFunction : function (success, error) {
+        cordova.exec(success, error, 'MathCalculator', 'testThreadFunction', []);
+    }
+  };
 
-module.exports.add = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'add', [arg0]);
-};
+  MathCalculator.ERROR_CODES = {
+    '0':'MATHCALCULATOR_NOT_INITIALIZED',
+    '1':'MATHCALCULATOR_NOT_LISTENING'
+  };
 
-module.exports.findUsbDevices = function (success, error) {
-    exec(success, error, 'MathCalculator', 'findUsbDevices', []);
-};
-
-module.exports.setUsbDevice = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'setUsbDevice', [arg0]);
-};
-
-module.exports.openUsbConnection = function (success, error) {
-    exec(success, error, 'MathCalculator', 'openUsbConnection', []);
-};
-
-module.exports.closeUsbConnection = function (success, error) {
-    exec(success, error, 'MathCalculator', 'closeUsbConnection', []);
-};
-
-
-module.exports.sendCommand = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'sendCommand', [arg0]);
-};
-
-module.exports.sendCommandAndWaitResponse = function (arg0, success, error) {
-    exec(success, error, 'MathCalculator', 'sendCommandAndWaitResponse', [arg0]);
-};
-
-module.exports.getUsbDevice = function (success, error) {
-    exec(success, error, 'MathCalculator', 'getUsbDevice', []);
-};
-
-module.exports.isUsbDeviceConnected = function (success, error) {
-    exec(success, error, 'MathCalculator', 'isUsbDeviceConnected', []);
-};
-
-module.exports.openAscanUsbConnection = function (success, error) {
-    exec(success, error, 'MathCalculator', 'openAscanUsbConnection', []);
-};
-
-module.exports.getAscan = function (success, error) {
-    exec(success, error, 'MathCalculator', 'getAscan', []);
-};
-
-module.exports.testFunction = function (success, error) {
-    exec(success, error, 'MathCalculator', 'testFunction', []);
-};
-
-module.exports.testThreadFunction = function (success, error) {
-    exec(success, error, 'MathCalculator', 'testThreadFunction', []);
-};
-
-
-
+  module.exports = MathCalculator; 
